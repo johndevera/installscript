e24f1b5fbf4d80f68d5f59f8c731a107827a8c02
John de Vera <john.devera@gmail.com>
2018-01-19 15:29:47 -0500

    include adobe acrobat

--- installscript.sh
+++ installscript.sh
@@ -75,7 +75,12 @@ sudo apt-get update
 sudo apt-get install vlc browser-plugin-vlc
 
 
-
+##################### INSTALL ADOBE ACROBAT
+sudo add-apt-repository "deb http://archive.canonical.com/ precise partner"
+sudo apt-get update
+sudo apt install adobereader-enu
+sudo add-apt-repository -r "deb http://archive.canonical.com/ precise partner"
+sudo apt update
 
 ###################### CODE TO BE ADDED TO ~/.bashrc
 echo 'source ~/Dropbox/devel/setup.bash' >> ~/.bashrc
